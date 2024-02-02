e3e0633616ff95edbe2fb78e757afb15bc1dd5c9
Daniel Odoj <danielodoj321@gmail.com>
2024-02-02 02:12:44 +0100

    dodalem css i obrazki

--- Conservation/urls.py
+++ Conservation/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from ConservationForum.views import HomeView, SpeciesView, ForumView, AddPostView
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     path('forum', ForumView.as_view(), name='forum'),
     path('addpost', AddPostView.as_view(), name='addPost'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
